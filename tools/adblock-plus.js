840ad18d7ba53885e3c0c4209e4c677d74f78983
Ash Kyd <ash@kyd.com.au>
2020-01-04 17:22:17 +1000

    add build step

---
+++ tools/adblock-plus.js
@@ -0,0 +1,14 @@
+const package = require('../package.json');
+const path = require('path');
+const fs = require("fs");
+const list = fs.readFileSync(path.join(__dirname, "../list.tsv"), "utf8");
+const parsedList = list.split("\n").map(row => row.split("\t"));
+
+console.log(`[Adblock Plus 2.0]
+! Version: ${package.version}
+! Title: ${package.name}
+! Last modified: ${new Date()}
+! Expires: 30 days (update frequency)
+! Homepage: ${package.homepage}
+!
+${parsedList.map(([name,domain]) => domain).join()}##body`);
